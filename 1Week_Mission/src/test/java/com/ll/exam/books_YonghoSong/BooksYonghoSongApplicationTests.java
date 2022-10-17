4d01b91bc6eeec191ce06a94c0ba4d4e88de0c9b
YongHo Song <spphire2@gmail.com>
2022-10-17 11:12:24 +0900

    Settings

---
+++ 1Week_Mission/src/test/java/com/ll/exam/books_YonghoSong/BooksYonghoSongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.books_YonghoSong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksYonghoSongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
