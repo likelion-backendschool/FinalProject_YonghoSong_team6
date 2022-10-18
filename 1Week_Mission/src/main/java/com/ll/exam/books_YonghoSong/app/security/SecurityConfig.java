f2e53e18aeece6fe132c3d62e95007c29778b3ec
YongHo Song <spphire2@gmail.com>
2022-10-18 15:17:57 +0900

    FEAT : 시큐리티 설정
    기본적인 설정

---
+++ 1Week_Mission/src/main/java/com/ll/exam/books_YonghoSong/app/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.ll.exam.books_YonghoSong.app.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        http
+                .authorizeRequests(
+                        authorizeRequests -> authorizeRequests
+                                .antMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                                .antMatchers("/member/login", "/member/join").permitAll()
+                                .antMatchers("/**").permitAll()
+                                .anyRequest()
+                                .authenticated() // 최소자격 : 로그인
+                )
+                .cors().disable() // 타 도메인에서 API 호출 가능
+                .csrf().disable() // CSRF 토큰 끄기
+                .httpBasic().disable() // httpBasic 로그인 방식 끄기
+                .formLogin().disable() // 폼 로그인 방식 끄기
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(STATELESS)
+                );
+
+        return http.build();
+    }
+}
