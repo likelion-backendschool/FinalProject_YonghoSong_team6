119429aae9ba70ea11a81499d7b40b57eae0b2a7
YongHo Song <spphire2@gmail.com>
2022-10-26 00:13:46 +0900

    OTHER : 폐기 2

--- 2Week_Mission/src/main/java/com/ll/exam/books_YonghoSong/app/product/cart/CartItemRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ll.exam.books_YonghoSong.app.product.cart;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-}
--- 2Week_Mission/src/main/java/com/ll/exam/books_YonghoSong/app/product/order/OrderService.java
+++
@@ -1,9 +0,0 @@
-package com.ll.exam.books_YonghoSong.app.product.order;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class OrderService {
-}
