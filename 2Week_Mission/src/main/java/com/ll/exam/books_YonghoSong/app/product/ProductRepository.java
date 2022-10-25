130ea0141c83578cf85021a3cbb42778e6a480e3
YongHo Song <spphire2@gmail.com>
2022-10-25 16:45:43 +0900

    FEAT : 빌드 오류 해결
     : memberContext 는 Bean 이 아님..

--- 2Week_Mission/src/main/java/com/ll/exam/books_YonghoSong/app/product/ProductRepository.java
+++ 2Week_Mission/src/main/java/com/ll/exam/books_YonghoSong/app/product/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.ll.exam.books_YonghoSong.app.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
 }
