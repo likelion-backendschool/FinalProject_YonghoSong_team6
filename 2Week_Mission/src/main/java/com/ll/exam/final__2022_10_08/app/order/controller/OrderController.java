5cd51cf016bbfc68fa6d308e8a02f10af655e1be
YongHo Song <spphire2@gmail.com>
2022-10-26 16:17:19 +0900

    FEAT : 주문 부분 컨트롤러 1
    리스팅, 상세, 생성, 결제

--- 2Week_Mission/src/main/java/com/ll/exam/final__2022_10_08/app/order/controller/OrderController.java
+++ 2Week_Mission/src/main/java/com/ll/exam/final__2022_10_08/app/order/controller/OrderController.java
@@ -1,18 +1,68 @@
 package com.ll.exam.final__2022_10_08.app.order.controller;
 
+import com.ll.exam.final__2022_10_08.app.base.rq.Rq;
+import com.ll.exam.final__2022_10_08.app.member.entity.Member;
+import com.ll.exam.final__2022_10_08.app.order.entity.Order;
+import com.ll.exam.final__2022_10_08.app.order.service.OrderService;
+import com.ll.exam.final__2022_10_08.app.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class OrderController {
     /**
      * todo
-     * POST /order/create
      * GET /order/list
      * GET /order/{id}
-     * POST /order/{id}/cancel
+     * POST /order/create
      * POST /order/{id}/pay
-     * POST /order/{id}/refund
+     * POST /order/{id}/cancel X
+     * POST /order/{id}/refund X
      */
+
+    private final Rq rq;
+    private final OrderService orderService;
+    private final ProductService productService;
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/order/list")
+    public String orderList(Model model){
+        Member member = rq.getMember();
+        List<Order> orders = orderService.findAllOrderByMember(member);
+        model.addAttribute("orders",orders);
+        return "/order/list";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/order/{id}")
+    public String orderDetail(Model model, @RequestParam long id){
+        Order order = orderService.findById(id);
+        model.addAttribute("order",order);
+        return "/order/"+id;
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/order/create")
+    public String createOrder() {
+        Member member = rq.getMember();
+        Order order = orderService.createFromCart(member);
+        return Rq.redirectWithMsg("/order/" + order.getId(), "주문이 생성되었습니다.");
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/order/{id}/pay")
+    public String payOrder(@RequestParam long id) {
+        Order order = orderService.findById(id);
+        orderService.payByRestCashOnly(order);
+        return Rq.redirectWithMsg("/order/"+id, "주문 결제가 완료 되었습니다.");
+    }
+
 }
