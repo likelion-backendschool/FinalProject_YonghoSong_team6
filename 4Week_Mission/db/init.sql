552c67b23489f026f545c22f1e1aac0c534672ec
YongHo Song <spphire2@gmail.com>
2022-11-07 16:51:50 +0900

    4주차 시작코드

---
+++ 4Week_Mission/db/init.sql
@@ -0,0 +1,9 @@
+# 테스트 DB 생성
+DROP DATABASE IF EXISTS books4__final__test;
+CREATE DATABASE books4__final__test;
+USE books4__final__test;
+
+# 개발 DB 생성
+DROP DATABASE IF EXISTS books4__final__dev;
+CREATE DATABASE books4__final__dev;
+USE books4__final__dev;
