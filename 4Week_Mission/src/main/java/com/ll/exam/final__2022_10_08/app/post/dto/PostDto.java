a6d277aae63b1781a2f6a801a9e8a573aede8a93
YongHo Song <spphire2@gmail.com>
2022-11-08 17:01:28 +0900

    FEAT : PostDto 추가

---
+++ 4Week_Mission/src/main/java/com/ll/exam/final__2022_10_08/app/post/dto/PostDto.java
@@ -0,0 +1,30 @@
+package com.ll.exam.final__2022_10_08.app.post.dto;
+
+import com.ll.exam.final__2022_10_08.app.post.entity.Post;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@Builder
+@RequiredArgsConstructor
+public class PostDto {
+    Long id;
+    String subject;
+    String content;
+    String contentHtml;
+
+    public static PostDto fromEntity(@NotNull Post post)
+    {
+        return PostDto.builder()
+                .id(post.getId())
+                .subject(post.getSubject())
+                .content(post.getContent())
+                .contentHtml(post.getContentHtml())
+                .build();
+    }
+}
